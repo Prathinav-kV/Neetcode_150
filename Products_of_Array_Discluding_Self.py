2d5edc2720392f1a628929f975829d9e9e536b77
Prathinav-kV <prathinav.kv@gmail.com>
2024-06-10 18:27:19 -0400

    Products of Array Discluding Self

---
+++ Products_of_Array_Discluding_Self.py
@@ -0,0 +1,41 @@
+# Products of Array Discluding Self
+# Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+
+# Each product is guaranteed to fit in a 32-bit integer.
+
+# Follow-up: Could you solve it in 
+# 𝑂
+# (
+# 𝑛
+# )
+# O(n) time without using the division operation?
+
+# Example 1:
+
+# Input: nums = [1,2,4,6]
+
+# Output: [48,24,12,8]
+# Example 2:
+
+# Input: nums = [-1,0,1,2,3]
+
+# Output: [0,-6,0,0,0]
+# Constraints:
+
+# 2 <= nums.length <= 1000
+# -20 <= nums[i] <= 20
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        final = []
+
+        for i in range(len(nums)):
+            prod = 1
+            for j in range(len(nums)):
+
+                if i != j:
+                    prod *= nums[j]
+            final.append(prod)
+        return final
